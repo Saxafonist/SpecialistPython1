65bdf1e9066bdb8535278304f663b82e28d2395a
Saxafonist <107915236+Saxafonist@users.noreply.github.com>
2022-06-27 19:52:04 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,8 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+numbers = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+quantity = 0
+for i in numbers:
+    quantity = quantity + i
+print(quantity)
